a3ecd9e7247866c202eece1fc104cef755dd2d40
Muttsu <muttsu@outlook.com>
2017-04-03 14:28:09 -0400

    Update mod_correction
    work with on board PID module

--- main.c
+++ main.c
@@ -9,7 +9,40 @@
 #include "config.h" //includes general configuration and declarations
 
 void main() {
-    init(); //included in bsp.c
+    init(); //included in bsp.c    
+    //IR[3] = 1;
+    //IR[4] = 1;
+    
+    PID_Init();
+    long val0 = 0;
+    uint pos = 0;
+    val0 = GetPID();
+    pos = GetPos();
+    long val1 = 0;
+    val1 = GetPID();
+    long val2 = 0;
+    val2 = GetPID();
+    extern char IR[];
+    IR[6] = IR[7] = 0;
+    IR[3] = IR[4] = 1;
+    pos= GetPos();
+    long val3 = 0;
+    val3 = GetPID();
+    long val4 = 0;
+    val4 = GetPID();
+    long val5 = 0;
+    val5 = GetPID();
+    long val6 = 0;
+    val6 = GetPID();
+    long val7 = 0;
+    val7 = GetPID();
+    long val8 = 0;
+    val8 = GetPID();
+    long val9 = 0;
+    val9 = GetPID();
+    long vala = 0;
+    vala = GetPID();
+    //value = GetError();
     for(;;){
         
     }
--- mod_correction.c
+++ mod_correction.c
@@ -5,11 +5,11 @@
 
 #include "config.h"
 
-#define MULTIPLIER 1000
-#define SET_POINT 3500
+#define MULTIPLIER 6
+#define SET_POINT 21
 #define ERROR -1
 
-char IR[] = {0,0,0,0,0,0,0,0};
+char IR[] = {0,0,0,0,0,0,1,1};
 
 void UpdateIR(){
     IR[0] = IR0;
@@ -23,7 +23,7 @@ void UpdateIR(){
 }
 
 int CalcError(){
-    static uint meanSum,sum,proportional, integral, differential, _proportional, error;
+    static uint meanSum,sum,proportional,integral,differential,_proportional,error;
     for (char i = 0; i < 8; i++){
         meanSum += IR[i]*i*MULTIPLIER;
         sum += IR[i];
@@ -39,4 +39,47 @@ int CalcError(){
     else{
         return ERROR;
     }
+}
+
+void PID_Init(){
+    PID1CON = 0b10000101;
+    PID1K1 = (unsigned short)(Kp + Ki*T + Kd/T);
+    PID1K2 = (unsigned short)(0 - (Kp + 2*Kd/T));
+    PID1K3 = (unsigned short)Kd/T;
+    PID1SET = SET_POINT;
+}
+
+uint GetPos(){
+    static uint meanSum,sum,value;
+    meanSum = sum = 0;
+    for (char i = 0; i < 8; i++){
+        meanSum += IR[i]*i*MULTIPLIER;
+        sum += IR[i];
+    }
+    if(sum==0){
+        return (value<21?0:42);
+    }
+    value = meanSum/sum;
+    return value;
+}
+
+long GetPID(){
+    
+    uint pos = GetPos();
+    PID1SETH = (uint)(SET_POINT >> 8);
+    PID1SETL = (uint)(SET_POINT & 0x00ff);
+    PID1INH = (uint)(pos >> 8);
+    PID1INL = (uint)(pos & 0x00ff);
+    
+    while(PID1CONbits.PID1BUSY);
+    
+    long value = PID1OUTHH;
+    value <<= 8;
+    value = PID1OUTHL;
+    value <<= 8;
+    value = PID1OUTLH;
+    value <<= 8;
+    value = PID1OUTLL;
+    
+    return value;
 }
