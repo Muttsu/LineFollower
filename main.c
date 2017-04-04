3082e73ea7d46b363719137d111eb80877e7d438
Muttsu <muttsu@outlook.com>
2017-04-04 16:47:54 -0400

    update to main.c

--- main.c
+++ main.c
@@ -20,10 +20,12 @@ void main() {
          
             //Correction module main routine
             if(!PID1CONbits.BUSY)correction = GetPID();
-            
             StartPID();
             
-            cycle = 0;
+            //Entry point for servo motor control using correction value obtained from PID
+            
+            
+            cycle ^= 1;
         }        
     }
 }
@@ -31,8 +33,9 @@ void main() {
 void interrupt ISR(){
     if(TMR0IF){
         
-        //main routine
-        cycle = 1;
+        //main routine timing (5ms)
+        cycle ^= 1;// when the cycle is not completed in 5ms, it will skip the next cycle;
+        //it it is yet not completed, the 2nd next cycle will not be skipped
         
         TMR0IF = 0;
         TMR0 = TMR0_INIT;
